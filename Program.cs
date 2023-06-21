6142882cabc15df87861845d2c6c5846d1d2318d
Dmitriy <35215775+DimaTer89@users.noreply.github.com>
2023-06-21 19:45:20 +0300

    update solution and add read.me

--- Program.cs
+++ Program.cs
@@ -5,16 +5,19 @@
 		private class MinHeap
 		{
 			private readonly List<int> heap;
+			private readonly Dictionary<int, int> mapIndex;
 
 			private void Swap(int i, int j)
 			{
+				mapIndex[heap[i]] = j;
+				mapIndex[heap[j]] = i;
 				(heap[i], heap[j]) = (heap[j], heap[i]);
 			}
 
 			private void SiftUp(int curIdx)
 			{
 				int parentIdx = (curIdx - 1) / 2;
-				while (parentIdx >= 0 && heap[parentIdx] < heap[curIdx])
+				while (parentIdx >= 0 && heap[parentIdx] > heap[curIdx])
 				{
 					Swap(parentIdx, curIdx);
 					curIdx = parentIdx;
@@ -49,21 +52,29 @@
 			public MinHeap()
 			{
 				heap = new List<int>();
+				mapIndex = new Dictionary<int, int>();
 			}
 
 			public void Add(int val)
 			{
-				heap.Add(val);
-				SiftUp(heap.Count - 1);
+				if(!mapIndex.ContainsKey(val))
+				{
+					heap.Add(val);
+					mapIndex.Add(val, heap.Count - 1);
+					SiftUp(heap.Count - 1);
+				}
 			}
 
-			public int Remove()
+			public void Remove(int val)
 			{
-				Swap(0, heap.Count - 1);
-				int removed = heap[^1];
-				heap.RemoveAt(heap.Count - 1);
-				SiftDown(0, heap.Count - 1);
-				return removed;
+				if (mapIndex.ContainsKey(val))
+				{
+					int removeIdx = mapIndex[val];
+					Swap(removeIdx, heap.Count - 1);
+					heap.RemoveAt(heap.Count - 1);
+					mapIndex.Remove(val);
+					SiftDown(removeIdx, heap.Count - 1);
+				}
 			}
 
 			public int GetMinimum()
@@ -71,8 +82,10 @@
 				return heap[0];
 			}
 		}
+
 		static void Main(string[] args)
 		{
+			List<int> minimumHeap = new();
 			MinHeap minHeap = new MinHeap();
 			int queries = Convert.ToInt32(Console.ReadLine().TrimEnd());
 			for (int query = 1; query <= queries; ++query)
@@ -84,13 +97,15 @@
 						minHeap.Add(Convert.ToInt32(data[1]));
 						break;
 					case "2":
-						minHeap.Remove();
+						minHeap.Remove(Convert.ToInt32(data[1]));
 						break;
 					default:
-						Console.WriteLine(minHeap.GetMinimum());
+						minimumHeap.Add(minHeap.GetMinimum());
 						break;
 				}
 			}
+			Console.Clear();
+			Console.WriteLine(string.Join('\n', minimumHeap));
 		}
 	}
 }
--- README.md
+++ README.md
@@ -1,1 +1,41 @@
 # QHEAP1
+
+This question is designed to help you get a better understanding of basic heap operations.
+
+There are 3 types of query:
+
+"1 v" - Add an element v to the heap.
+"2 v" - Delete the element v from the heap.
+"3" - Print the minimum of all the elements in the heap.
+NOTE: It is guaranteed that the element to be deleted will be there in the heap. Also, at any instant, only distinct elements will be in the heap.
+
+Input Format
+
+The first line contains the number of queries, Q.
+Each of the next Q lines contains one of the 3 types of query.
+
+Constraints
+1 <= Q <= 10^5
+-10^9 <= V <= 10^9
+
+Output Format
+
+For each query of type 3, print the minimum value on a single line.
+
+Sample Input
+
+STDIN       Function
+-----       --------
+5           Q = 5
+1 4         insert 4
+1 9         insert 9
+3           print minimum
+2 4         delete 4
+3           print minimum
+Sample Output
+
+4  
+9 
+Explanation
+
+After the first 2 queries, the heap contains {4, 9}. Printing the minimum gives 4 as the output. Then, the 4th query deletes 4 from the heap, and the 5th query gives 9 as the output.
